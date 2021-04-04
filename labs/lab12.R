628265a48c95598125625b16ad9fe60679337fd5
José C. Pernías <jcpernias@gmail.com>
2021-04-04 11:40:34 +0200

    Update lab12

--- labs/lab12.R
+++ labs/lab12.R
@@ -1,60 +1,67 @@
-library(tidyverse)
-library(wooldridge)
+library(ec1027)
 library(dynlm)
-library(texreg)
-library(broom)
-library(lmtest)
-library(sandwich)
+library(ggplot2)
 
-db <- earns %>%
-  select(year, hrwage, outphr) %>%
-  mutate(lhrwage = log(hrwage),
-         loutphr = log(outphr), 
-         d_lhrwage = lhrwage - lag(lhrwage),
-         d_loutphr = loutphr - lag(loutphr),
-         d2_loutphr = d_loutphr - lag(d_loutphr)) %>%
-  ts(start = 1947)
+# library(tidyverse)
+# library(wooldridge)
+# library(texreg)
+# library(broom)
+# library(lmtest)
+# library(sandwich)
 
-db_long <- tidy(db)
-ggplot(db_long %>% filter(series == "lhrwage"),
-       aes(x = index, y = value, group = series)) + 
-  geom_line()
+data(earns)
 
-ggplot(db_long %>% filter(series == "loutphr"),
-       aes(x = index, y = value, group = series)) + 
-  geom_line()
+earns <- within(earns, {
+  hrwage <- wkearns / wkhours
+  lhrwage <- log(hrwage)
+  loutphr <- log(outphr)
+})
 
-ggplot(db_long %>% filter(series == "d_lhrwage", index != 1947),
-       aes(x = index, y = value, group = series)) + 
-  geom_line()
+db <- zoo(earns, earns$year)
 
-ggplot(db_long %>% filter(series == "d_loutphr", index != 1947),
-       aes(x = index, y = value, group = series)) + 
-  geom_line()
-
-model_1 <- dynlm(lhrwage ~ loutphr + trend(lhrwage), data = db, start = 1949)  
-screenreg(model_1, digits = 4)
-bgtest(model_1)
-
-uhat_1 <- resid(model_1)
-ggplot(tidy(uhat_1),
-       aes(x = index, y = value)) + 
-  geom_line()
-
-model_2 <- dynlm(d_lhrwage ~ d_loutphr, data = db, start = 1949)  
-screenreg(model_2, digits = 4)
-bgtest(model_2)
+autoplot(db$lhrwage)
+autoplot(db$loutphr)
 
 
-model_3 <- dynlm(d_lhrwage ~ d_loutphr + L(d_loutphr), data = db, start = 1949)  
-screenreg(model_3, digits = 4)
-bgtest(model_3)
+model1 <- dynlm(lhrwage ~ loutphr + year, data = db, start = 1949)
+summary(model1)
+uhat1 <- resid(model1)
+autoplot(uhat1)
+db_ar <- merge(db, uhat1, fill = 0)
+aux_ar <- update(model1, uhat1 ~ . + L(uhat1), data = db_ar)
+summary(aux_ar)
 
 
-model_4 <- dynlm(d_lhrwage ~ d2_loutphr  + L(d_loutphr), data = db, start = 1949)  
-screenreg(model_4, digits = 4)
-bgtest(model_4)
+db$ghrwage <- diff(db$lhrwage)
+db$goutphr <- diff(db$loutphr)
 
-model_5 <- dynlm(d_lhrwage ~ d_loutphr  + L(d_loutphr) + L(d_loutphr, 2), data = db, start = 1949)  
-screenreg(model_5, digits = 4)
-bgtest(model_5)
+autoplot(db$ghrwage)
+autoplot(db$goutphr)
+
+model2 <- dynlm(ghrwage ~ goutphr, data = db, start = 1949)
+summary(model2)
+
+db_ar <- merge(db, uhat2 = resid(model2), fill = 0)
+aux_ar <- update(model2, uhat2 ~ . + L(uhat2), data = db_ar)
+summary(aux_ar)
+
+model3 <- update(model2, . ~ . + L(goutphr))
+summary(model3)
+
+db$dgoutphr <- diff(db$goutphr)
+
+model4 <- dynlm(ghrwage ~ dgoutphr  + L(goutphr), data = db, start = 1949)
+summary(model4)
+
+
+model5 <- update(model3, . ~ . + L(goutphr, 2), data = db, start = 1949)
+summary(model5)
+
+model_lst <- list("No lags" = model2,
+                  "1 lag" = model3,
+                  "2 lags" = model5)
+vapply(model_lst, AIC, numeric(1))
+vapply(model_lst, BIC, numeric(1))
+
+
+
