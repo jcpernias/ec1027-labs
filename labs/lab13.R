e0e2f2ab6a5beeb85808d709be0b2ad9c341de46
José C. Pernías <jcpernias@gmail.com>
2021-04-04 20:18:38 +0200

    Update lab13

--- labs/lab13.R
+++ labs/lab13.R
@@ -1,133 +1,91 @@
-library(tidyverse)
-library(glue)
-library(wooldridge)
+library(ec1027)
+library(ggplot2)
 library(dynlm)
-library(texreg)
-library(broom)
-library(lmtest)
 library(sandwich)
-library(prais)
-library(orcutt)
 
-tr2 <- traffic2 %>% 
-  select(totacc, fatacc, prcfat, beltlaw, spdlaw, wkends, unem) %>%
-  ts(start = c(1981, 1), frequency = 12)
+data(traffic2)
 
-ts_dates <- function(ts) {
-  y <- as.integer(time(ts))
-  m <- cycle(ts)
-  lubridate::make_date(year = y, month = m)
+monthly_index <- function(year, month) {
+  yearmon(year + (month - 1) / 12)
 }
 
-ts_to_df <- function(ts) {
-  df <- as_tibble(ts) %>% mutate(.Index = ts_dates(ts))
-  df
-}
+v <- traffic2$month
+N <- length(v)
+values <- unique(v)
+lst <- lapply(values, function(x) (v == x) + 0)
+names(lst) <- paste0("seas", formatC(1:12, width = 2, flag = "0"))
 
-ts_pull <- function(data, var) {
-  v <- rlang::as_name(rlang::ensym(var))
-  data[, v]
-}
+traffic2 <- data.frame(traffic2, lst)
 
+idx <- with(traffic2, monthly_index(year, month))
 
-ts_plot <- function(data, x) {
-  df <- ts_to_df(data)
-  plot_db <- df %>% select(date = .Index, {{ x }}) 
-  ggplot(plot_db, aes(x = date, y = {{ x }})) +
-    geom_line() +
-    scale_x_date(date_labels = "%b %Y") +
-    xlab(NULL)
-}
+traffic2 <- within(traffic2, {
+  trend <- (year - 1981) * 12 + month - 1
+  prcfat <- 100 * fatacc / totacc
+})
 
-ts_lag <- function(ts, k = 1, fill = NA) {
-  ts_start <- start(ts)
-  ts_end <- end(ts)
-  lagged <- window(stats::lag(ts, k = -k), 
-                   start = ts_start, end = ts_end, extend = TRUE)
-  window(lagged, start = ts_start, end = ts_start) <- fill 
-  colnames(lagged) <- glue("L{k}_{colnames(ts)}")
-  lagged
-}
+db <- zoo(traffic2, idx)
 
-ts_diff <- function(ts, k = 1, fill = NA) {
-  ts_start <- start(ts)
-  ts_end <- end(ts)
-  lagged <- ts_lag(ts, k = k)
-  d <- ts - lagged
-  colnames(d) <- glue("D{k}_{colnames(ts)}")
-  d
-}
+autoplot(db$prcfat)
 
-ts_det <- function(ts) {
-  seas_names <- function(x, width) {
-    glue("seas{formatC(x, flag = '0', width = width)}")
-  }
-
-  seas_fn <- function(x, y) {
-    ifelse(x == y, 1, 0)
-  }
-  
-  det <- cbind(time(ts) - start(ts)[1], 
-                  outer(cycle(ts), 1:12, seas_fn))
-  colnames(det) <- c("t", seas_names(1:12, 2))
-  det  
-}
-
-ts_merge <- function(...) {
-  args <- list(...)
-  all <- do.call(ts.union, args)
-  all_names <- unlist(lapply(args, colnames))
-  colnames(all) <- all_names
-  all
-}
-
-
-tr2_det <- ts_det(tr2)
-L1_tr2 <- ts_lag(tr2)
-D1_tr2 <- ts_diff(tr2)
-tr2_plus <- ts_merge(tr2, tr2_det, L1_tr2, D1_tr2)
-
-
-
-ts_plot(tr2, prcfat)
-
-zz <- ts_pull(tr2, prcfat)
-
-frml_1 <- prcfat ~ t + seas02 + seas03 + seas04 + seas05 + 
+frml1 <- prcfat ~ trend + seas02 + seas03 + seas04 + seas05 +
   seas06 + seas07 + seas08 + seas09 + seas10 + seas11 + seas12 +
   wkends + unem + spdlaw + beltlaw
-model1 <- dynlm(frml_1, data = tr2_plus)
-screenreg(model1, digits = 4)
+model1 <- dynlm(frml1, data = db)
+summary(model1)
 
-uhat_1 <- resid(model1)
-
-ggplot(tidy(uhat_1), aes(x = index, y = value)) +
-  geom_line() + 
-  scale_x_yearmon()
+uhat1 <- resid(model1)
+autoplot(uhat1)
 
 ## AR(1) test
-L.uhat_1 <- window(stats::lag(uhat_1, k = -1), start = c(1981, 1), extend = TRUE)
-window(L.uhat_1, start = c(1981, 1), end = c(1981, 1)) <- 0
-ar1_db <- ts.intersect(uhat_1, L.uhat_1, tr2)
-colnames(ar1_db) <- c("uhat_1", "L.uhat_1", colnames(tr2))
-ar1_frml <- update(frml_1, uhat_1 ~ . + L.uhat_1)
-ar1_test <- dynlm(ar1_frml, data = ar1_db)
-screenreg(ar1_test, digits = 4)
+uhat <- resid(model1)
+db_u <- merge(uhat, uhat_1 = lag(uhat, k = -1), fill = 0)
+db_ar <- merge(db, db_u)
+aux_ar <- update(model1, uhat ~ . + uhat_1, data = db_ar)
+summary(aux_ar)
 
-summary(dynlm(uhat_1 ~ L.uhat_1, data = ar1_db))
+summary(dynlm(uhat1 ~ uhat_1, data = db_ar))
 
-coeftest(model1, vcov. = NeweyWest)
+coef_table(model1, vce = "NW")
 
-co <- cochrane.orcutt(model1)
+lmtest::coeftest(model1, vcov. = sandwich::NeweyWest, prewhite = FALSE)
+
+## CO estimation
+model <- model1
+mf <- model$model
+y <- model.response(mf)
+mm <- as.zoo(model.matrix(model, mf), index(y))
+
+uhat <- resid(model)
+
+db_rho <- merge(uhat, uhat_1 = lag(uhat, k = -1))
+mod_rho <- lm(uhat ~ uhat_1 - 1, data = db_rho)
+rho <- coef(mod_rho)[[1]]
+rho
+
+Xtr <- mm - rho * lag(mm, k = -1)
+ytr <- y - rho * lag(y, k = -1)
+co <- dynlm(ytr ~ Xtr - 1)
+uhat <- y - mm %*% coef(co)
+
 summary(co)
 
-tidx <- time(tr2) - 1981
-seas <- as.matrix(outer(cycle(tr2), 1:12, function(x, y) {
-  ifelse(x == y, 1, 0)
-}))
-tr2_plus <- ts.intersect(tr2, seas, tidx)
-colnames(tr2_plus) <- c(colnames(tr2), month.abb, "t")
-frml_pw <- prcfat ~ t + 
-  Feb + Mar + Apr + May + Jun + Jul + Aug + Sep + Oct + Nov + Dec + 
-  wkends + unem + spdlaw + beltlaw
-pw <- prais_winsten(frml_pw, data = tr2_plus)
+## PW estimation
+uhat <- resid(model)
+
+db_rho <- merge(uhat, uhat_1 = lag(uhat, k = -1))
+mod_rho <- lm(uhat ~ uhat_1 - 1, data = db_rho)
+rho <- coef(mod_rho)[[1]]
+rho
+
+Xtr <- rbind(sqrt(1-rho^2) * mm[1, ], mm - rho * lag(mm, k = -1))
+ytr <- c(sqrt(1-rho^2) * y[1], y - rho * lag(y, k = -1))
+co <- dynlm(ytr ~ Xtr - 1)
+uhat <- y - mm %*% coef(co)
+
+summary(co)
+
+
+pw <- prais::prais_winsten(frml1, data = db)
+summary(pw)
+
