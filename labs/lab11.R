82be82a5eef2645ef26483ef2850dbe21c209526
José C. Pernías <jcpernias@gmail.com>
2021-04-04 10:13:33 +0200

    Update lab11

--- labs/lab11.R
+++ labs/lab11.R
@@ -1,66 +1,62 @@
-library(tidyverse)
-library(wooldridge)
+library(ec1027)
 library(dynlm)
-library(texreg)
-library(broom)
+library(ggplot2)
 
-years <- 1947:1988
-db <- hseinv %>%
-  select(inv, pop, price) %>%
-  mutate(year = 1947:1988,
-         invpc = inv / pop,
-         l_invpc = log(invpc),
-         l_price = log(price),
-         dl_invpc = l_invpc - lag(l_invpc),
-         dl_price = l_price - lag(l_price)) %>%
-  filter(year != 1947) %>%
-  ts(start = 1948, frequency = 1)
+#library(tidyverse)
+#library(wooldridge)
+#library(texreg)
+#library(broom)
 
+data(hseinv)
 
-db_long <- tidy(db)
+hseinv <- within(hseinv, {
+  invpc <- inv / pop
+  linvpc <- log(invpc)
+  lprice <- log(price)
+})
 
-ggplot(db_long %>% filter(series == "l_invpc"),
-       aes(x = index, y = value, group = series)) +
-  geom_line()
-
-ggplot(db_long %>% filter(series == "l_price"),
-       aes(x = index, y = value, group = series)) +
-  geom_line()
+db <- zoo(hseinv, hseinv$year)
 
 
-model_1 <- dynlm(l_invpc ~ l_price, data = db)
-model_2 <- dynlm(l_invpc ~ l_price + trend(l_invpc), data = db)
-screenreg(list(model_1, model_2), digits = 4)
+autoplot(db$linvpc)
+autoplot(db$lprice)
 
-invpc_ar <- dynlm(l_invpc ~ L(l_invpc, 1), data = db)
+model1 <- dynlm(linvpc ~ lprice, data = db)
+summary(model1)
+
+model2 <- dynlm(linvpc ~ lprice + year, data = db)
+summary(model2)
+
+invpc_ar <- dynlm(linvpc ~ L(linvpc, 1), data = db)
 coef(invpc_ar)
-invpc_art <- update(invpc_ar, . ~ . + trend(l_invpc))
+invpc_art <- update(invpc_ar, . ~ . + year)
 coef(invpc_art)
 
-price_ar <- dynlm(l_price ~ L(l_price, 1), data = db)
+price_ar <- dynlm(lprice ~ L(lprice, 1), data = db)
 coef(price_ar)
-price_art <- update(price_ar, . ~ . + trend(l_price))
+price_art <- update(price_ar, . ~ . + year)
 coef(price_art)
 
 
-ggplot(db_long %>% filter(series == "dl_invpc"),
-       aes(x = index, y = value, group = series)) +
-  geom_line()
+db$ginvpc <- diff(db$linvpc)
+db$gprice <- diff(db$lprice)
 
-ggplot(db_long %>% filter(series == "dl_price"),
-       aes(x = index, y = value, group = series)) +
-  geom_line()
+head(db)
 
+autoplot(db$ginvpc)
+autoplot(db$gprice)
 
-model_3 <- dynlm(l_invpc ~ dl_price + trend(l_invpc), data = db)
-screenreg(list(model_1, model_2, model_3))
+model3 <- dynlm(linvpc ~ gprice + year, data = db, start = 1948)
+summary(model3)
 
-invpc_t <- dynlm(l_invpc ~ trend(l_invpc), data = db)
-linvpc_not <- resid(invpc_t) 
+invpc_t <- dynlm(linvpc ~ year, data = db)
+linvpc_not <- resid(invpc_t)
 
-model_4 <- dynlm(linvpc_not ~ dl_price , data = db)
-screenreg(list(model_3, model_4), digits = 4)
+model4 <- dynlm(linvpc_not ~ gprice + year, data = db, start = 1948)
+summary(model4)
 
-model_5 <- dynlm(dl_invpc ~ dl_price + trend(l_invpc) , data = db)
-model_6 <- dynlm(dl_invpc ~ dl_price, data = db)
-screenreg(list(model_5, model_6), digits = 4)
+model5 <- dynlm(ginvpc ~ gprice + year, data = db, start = 1948)
+summary(model5)
+
+model6 <- dynlm(ginvpc ~ gprice, data = db, start = 1948)
+summary(model6)
